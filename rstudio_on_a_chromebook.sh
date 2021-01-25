3b1975b98c51f4a7b9c55e96ac775c8b3edf5705
Aaron C Spike <aaron@ekips.org>
2021-01-25 15:02:10 -0600

    initial draft of install script

---
+++ rstudio_on_a_chromebook.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Add R package repository
+. /etc/os-release
+echo "deb https://cran.rstudio.com/bin/linux/debian $VERSION_CODENAME-cran35/" | sudo tee -a /etc/apt/sources.list
+
+# Upgrade all OS packages
+sudo apt update && sudo apt upgrade
+
+# Install R and RStudio dependencies
+sudo apt install r-base r-base-dev libclang-7-dev libclang-dev libpq5 libnss3
+
+# Download and install RStudio
+curl -o rstudio.deb https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.4.1103-amd64.deb
+sudo dpkg -i rstudio.deb
