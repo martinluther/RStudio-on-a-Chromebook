03441568d5bedf81a458e990df30449e7296dadd
Aaron Spike <aaron@ekips.org>
2021-01-26 10:05:36 -0600

    Update rstudio_on_a_chromebook.sh
    Add symlink to reduce number of required clicks in RStudio file.choose() dialog

--- rstudio_on_a_chromebook.sh
+++ rstudio_on_a_chromebook.sh
@@ -13,3 +13,6 @@ sudo apt install r-base r-base-dev libclang-7-dev libclang-dev libpq5 libnss3
 # Download and install RStudio
 curl -o rstudio.deb https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.4.1103-amd64.deb
 sudo dpkg -i rstudio.deb
+
+# Add symlink to Chromebook Downloads folder in Linux home dir
+ln -s /mnt/chromeos/MyFiles/Downloads/ Downloads
