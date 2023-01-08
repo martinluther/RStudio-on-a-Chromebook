da3ee110a1cf681f21bb295af362d2790473a4ba
Aaron Spike <aaron@ekips.org>
2023-01-08 13:48:42 -0600

    Update rstudio_on_a_chromebook.sh
    Untested update for current versions in January 2023

--- rstudio_on_a_chromebook.sh
+++ rstudio_on_a_chromebook.sh
@@ -2,16 +2,17 @@
 
 # Add R package repository
 . /etc/os-release
-echo "deb https://cran.rstudio.com/bin/linux/debian $VERSION_CODENAME-cran35/" | sudo tee -a /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
+echo "deb https://cran.rstudio.com/bin/linux/debian $VERSION_CODENAME-cran40/" | sudo tee -a /etc/apt/sources.list
 
 # Upgrade all OS packages
 sudo apt update && sudo apt upgrade
 
 # Install R and RStudio dependencies
-sudo apt install r-base r-base-dev libclang-7-dev libclang-dev libpq5 libnss3
+sudo apt install r-base r-base-dev libclang-dev libpq5 libnss3
 
 # Download and install RStudio
-curl -o rstudio.deb https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.4.1103-amd64.deb
+curl -o rstudio.deb https://download1.rstudio.org/electron/bionic/amd64/rstudio-2022.12.0-353-amd64.deb
 sudo dpkg -i rstudio.deb
 
 # Add symlink to Chromebook Downloads folder in Linux home dir
